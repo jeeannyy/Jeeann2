6da05d711b7b494401214bae818b76e4bc94b06a
Jeeann <jeeann.kim@gmail.com>
2022-08-10 20:20:49 +0100

    Updates

---
+++ precache-manifest.c4fe01ffaebe433e1ec1f30eb9efd6a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb3cf5f372cfb64c226fe01e04fcc6d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ce6fee558154f657112e",
+    "url": "/Portfolio/static/css/2.8065d864.chunk.css"
+  },
+  {
+    "revision": "112e984acade7723f1ba",
+    "url": "/Portfolio/static/css/main.48b08df8.chunk.css"
+  },
+  {
+    "revision": "ce6fee558154f657112e",
+    "url": "/Portfolio/static/js/2.b1e5a49d.chunk.js"
+  },
+  {
+    "revision": "26cd29d98e307793cc8038aba14f2a12",
+    "url": "/Portfolio/static/js/2.b1e5a49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112e984acade7723f1ba",
+    "url": "/Portfolio/static/js/main.145f04df.chunk.js"
+  },
+  {
+    "revision": "27e57a3331b77bbb91ea",
+    "url": "/Portfolio/static/js/runtime-main.ff4f65f8.js"
+  }
+]);
