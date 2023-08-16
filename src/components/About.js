4ab3649ee85ef84d42ee872fad00e0760d3baf30
Jeeann <jeeann.kim@gmail.com>
2023-08-16 07:20:25 +0100

    added a new experience!

--- src/components/About.js
+++ src/components/About.js
@@ -1,53 +1,70 @@
-import React from "react";
-import "../styles/About.css";
-import FadeInSection from "./FadeInSection";
+import React from 'react';
+import '../styles/About.css';
+import FadeInSection from './FadeInSection';
 
 class About extends React.Component {
   constructor() {
     super();
     this.state = {
       expanded: true,
-      activeKey: "1"
+      activeKey: '1',
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
   handleSelect(eventKey) {
     this.setState({
-      activeKey: eventKey
+      activeKey: eventKey,
     });
   }
   render() {
     const one = (
       <p>
-        I am currently a <b>Frontend Developer</b> at {" "}<a href="https://www.chingu.io/"><b>Chingu</b></a> and <b>Course Instructor</b> at {" "}<a href="https://codefirstgirls.com/"><b>Code First Girls</b></a>. I studied {" "}<a href="https://eng.ssu.ac.kr/">Media Arts and Computer Science</a> at {" "}<a href="https://eng.ssu.ac.kr/"><b>Soongsil University</b></a> in Seoul, South Korea and finished my <b>Software Engineering course</b> at {" "}<a href="https://northcoders.com/"><b>Northcoders</b></a> in London, UK. Before starting this journey, I worked as a marketer and data operator specialist at tvN, Kakao and TikTok. 
+        I am currently a <b>Frontend Developer</b> at{' '}
+        <a href="https://www.storeboost.com/">
+          <b>StoreBoost</b>
+        </a>{' '}
+        and <b>Course Instructor</b> at{' '}
+        <a href="https://codefirstgirls.com/">
+          <b>Code First Girls</b>
+        </a>
+        . I studied{' '}
+        <a href="https://eng.ssu.ac.kr/">Media Arts and Computer Science</a> at{' '}
+        <a href="https://eng.ssu.ac.kr/">
+          <b>Soongsil University</b>
+        </a>{' '}
+        in Seoul, South Korea and finished my <b>Software Engineering course</b>{' '}
+        at{' '}
+        <a href="https://northcoders.com/">
+          <b>Northcoders</b>
+        </a>{' '}
+        in London, UK. Before starting this journey, I worked as a marketer and
+        data operator specialist at tvN, Kakao and TikTok.
       </p>
     );
     const two = (
       <p>
-        Outside of work, I'm interested in editing videos, painting, traveling, vintage fashion, R&B and jazz, and writing a diary.
+        Outside of work, I'm interested in editing videos, painting, traveling,
+        vintage fashion, R&B and jazz, and writing a diary.
       </p>
     );
     const three = (
       <p>
-        <b>Want to chat?</b> Shoot me a message at{" "}
-        <a href="jeeann.kim@gmail.com">
-        jeeann.kim@gmail.com
-        </a>{" "}
-        and let's talk.
+        <b>Want to chat?</b> Shoot me a message at{' '}
+        <a href="jeeann.kim@gmail.com">jeeann.kim@gmail.com</a> and let's talk.
       </p>
     );
     const desc_items = [one, two];
 
     const tech_stack = [
-      "Javascript ES6+",
-      "React.js",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-      "HTML & CSS",
+      'Javascript ES6+',
+      'React.js',
+      'TypeScript',
+      'Redux',
+      'Node.js',
+      'HTML & CSS',
     ];
 
-    const tech_items = tech_stack.map(stack => <li>{stack}</li>);
+    const tech_items = tech_stack.map((stack) => <li>{stack}</li>);
 
     return (
       <div id="about">
@@ -58,7 +75,7 @@ class About extends React.Component {
           <div className="about-content">
             <div className="about-description">
               {[one]}
-              {"Here are some technologies I have been working with:"}
+              {'Here are some technologies I have been working with:'}
               <ul className="tech-stack">
                 {tech_stack.map(function (tech_item, i) {
                   return (
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,11 +1,11 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import FadeInSection from "./FadeInSection";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import FadeInSection from './FadeInSection';
 
 const isHorizontal = window.innerWidth < 600;
 
@@ -49,32 +49,32 @@ function TabPanel(props) {
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
+  value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
   if (isHorizontal) {
     return {
       id: `full-width-tab-${index}`,
-      "aria-controls": `full-width-tabpanel-${index}`
+      'aria-controls': `full-width-tabpanel-${index}`,
     };
   } else {
     return {
-      id: `vertical-tab-${index}`
+      id: `vertical-tab-${index}`,
     };
   }
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    backgroundColor: "theme.palette.background.paper",
-    display: "flex",
-    height: 300
+    backgroundColor: 'theme.palette.background.paper',
+    display: 'flex',
+    height: 300,
   },
   tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`
-  }
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
 }));
 
 const JobList = () => {
@@ -82,68 +82,59 @@ const JobList = () => {
   const [value, setValue] = React.useState(0);
 
   const experienceItems = {
-    "Chingu": {
-      jobTitle: "Frontend Developer @",
-      duration: "JAN 2023 - Present",
+    StoreBoost: {
+      jobTitle: 'Frontend Developer @',
+      duration: 'FEB 2023 - Present',
+      desc: [
+        '* Building applications for the media planning platform for stakeholders',
+        'Tech Stack: React.js, TypeScript, Redux, Git, Material UI, Chart JS',
+      ],
+    },
+    'Code First Girls': {
+      jobTitle: 'Course Instructor @',
+      duration: 'NOV 2022 - Present',
+      desc: [
+        'Teaching online web development courses to women who are beginners in programming',
+        "Leading lectures and supporting other instructors' classes, assisting in their presentations",
+        'Tech Stack: JavaScript, Git, HTML, CSS',
+      ],
+    },
+
+    freeCodeCamp: {
+      jobTitle: 'Open Source Contributor @',
+      duration: 'OCT 2022 - Present',
+      desc: [
+        'Translating and reviewing freeCodeCamp tutorials from English to Korean for Korean developer community',
+      ],
+    },
+    Chingu: {
+      jobTitle: 'Frontend Developer @',
+      duration: 'JAN 2023 - Present',
       desc: [
         "* Collaborating with global web developers, contributing to our application's planning, designing, building, and deploying",
-        "Tech Stack: React.js, SASS, Zenhub, Git",
-      ]
+        'Tech Stack: React.js, SASS, Zenhub, Git',
+      ],
     },
-    "Code First Girls": {
-      jobTitle: "Course Instructor @",
-      duration: "NOV 2022 - Present",
+    'Athena Hackathon': {
+      jobTitle: 'Frontend Developer @',
+      duration: 'OCT 2022',
       desc: [
-        "Teaching online web development courses to women who are beginners in programming",
-        "Leading lectures and supporting other instructors' classes, assisting in their presentations",
-        "Tech Stack: JavaScript, Git, HTML, CSS",
-      ]
+        'Won the Best User Journey Prize from Valtech',
+        'Built an impact investment app that allows everyone to invest in socially and environmentally focused projects and companies through their daily spending activities, using our round-up feature.',
+        'Tech Stack: Python, Figma',
+      ],
     },
-    "Athena Hackathon": {
-      jobTitle: "Frontend Developer @",
-      duration: "OCT 2022",
+    TikTok: {
+      jobTitle: 'AI Data Operator Specialist @',
+      duration: 'SEP 2020 - FEB 2022',
       desc: [
-        "Won the Best User Journey Prize from Valtech",
-        "Built an impact investment app that allows everyone to invest in socially and environmentally focused projects and companies through their daily spending activities, using our round-up feature.",
-        "Tech Stack: Python, Figma",
-      ]
+        'Machine Translation Project with AI-Lab-NLP-Machine Learning team',
+        'Worked cooperatively with the internal and global team to build QA policies and created documentation related to Korean trends',
+        'Identified controversial cases and errors using the content review & discussion set-up within the team and to be updated stakeholders',
+        'Created an onboarding guide for newbies and trained them to onboard',
+        'Led and supported team-building activities in Korea',
+      ],
     },
-    "freeCodeCamp": {
-      jobTitle: "Open Source Contributor @",
-      duration: "OCT 2022 - Present",
-      desc: [
-        "Translating and reviewing freeCodeCamp tutorials from English to Korean for Korean developer community"
-      ]
-    },
-    "Pocket Forest": {
-      jobTitle: "Full Stack Software Engineer @",
-      duration: "2022",
-      desc: [
-        "Facilitated discussions with the team to access users' expectations and constructed wireframe",
-        "Developed front-end skeleton using modular, reusable React Native components and made final calls for production-level UI changes",
-        "Tech Stack: React Native, MongoDB",
-      ]
-    },
-    "NewNew" : {
-      jobTitle: "Full Stack Software Engineer @",
-      duration: "2022",
-      desc: [
-        "Architected a service of a ratings and reviews system to allow users to post reviews, view ratings, choose articles by categories and sort by likes, comments, date, and votes for a specific article",
-        "Built a news API with Node.js trying to mimic the real-world backend service which should provide information to the front-end architecture",
-        "Tech Stack: React.js, Node.js, Express, PSQL, CSS"
-      ]
-    },
-    "TikTok": {
-      jobTitle: "AI Data Operator Specialist @",
-      duration: "SEP 2020 - FEB 2022",
-      desc: [
-        "Machine Translation Project with AI-Lab-NLP-Machine Learning team",
-        "Worked cooperatively with the internal and global team to build QA policies and created documentation related to Korean trends",
-        "Identified controversial cases and errors using the content review & discussion set-up within the team and to be updated stakeholders",
-        "Created an onboarding guide for newbies and trained them to onboard",
-        "Led and supported team-building activities in Korea",
-      ]
-    }
   };
 
   const handleChange = (event, newValue) => {
@@ -153,8 +144,8 @@ const JobList = () => {
   return (
     <div className={classes.root}>
       <Tabs
-        orientation={!isHorizontal ? "vertical" : null}
-        variant={isHorizontal ? "fullWidth" : "scrollable"}
+        orientation={!isHorizontal ? 'vertical' : null}
+        variant={isHorizontal ? 'fullWidth' : 'scrollable'}
         value={value}
         onChange={handleChange}
         className={classes.tabs}
@@ -166,14 +157,14 @@ const JobList = () => {
       {Object.keys(experienceItems).map((key, i) => (
         <TabPanel value={value} index={i}>
           <span className="joblist-job-title">
-            {experienceItems[key]["jobTitle"] + " "}
+            {experienceItems[key]['jobTitle'] + ' '}
           </span>
           <span className="joblist-job-company">{key}</span>
           <div className="joblist-duration">
-            {experienceItems[key]["duration"]}
+            {experienceItems[key]['duration']}
           </div>
           <ul className="job-description">
-            {experienceItems[key]["desc"].map(function (descItem, i) {
+            {experienceItems[key]['desc'].map(function (descItem, i) {
               return (
                 <FadeInSection delay={`${i + 1}00ms`}>
                   <li key={i}>{descItem}</li>
