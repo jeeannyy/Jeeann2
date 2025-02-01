f68c6f6c7838ca0d0d632402891fc9fb699048fb
Jeeann <jeeann@storeboost.net>
2025-02-01 21:54:09 +0000

    refactor

--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -1,57 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Sidenav } from 'rsuite';
 import EmailRoundedIcon from '@material-ui/icons/EmailRounded';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
-
 import '../styles/SidebarNav.css';
-import 'react-typist/dist/Typist.css';
 import FadeInSection from './FadeInSection';
 
 const isMobile = window.innerWidth < 600;
 
 const SidebarNav = () => {
-	const [expanded, setExpanded] = useState(true);
-	const [activeKey, setActiveKey] = useState('1');
-
-	const handleSelect = (eventKey) => {
-		setActiveKey(eventKey);
-	};
-
 	const links = [
-		<a key='home' href='#intro'>
-			/home
-		</a>,
-		<a key='about' href='#about'>
-			/about
-		</a>,
-		<a key='experience' href='#experience'>
-			/experience
-		</a>,
-		<a key='projects' href='#projects'>
-			/software-creations
-		</a>,
+		{ label: '/home', href: '#intro' },
+		{ label: '/about', href: '#about' },
+		{ label: '/experience', href: '#experience' },
+		{ label: '/software-creations', href: '#projects' },
 	];
 
 	return (
 		<div className='sidebar-nav'>
 			{!isMobile && (
-				<Sidenav
-					expanded={expanded}
-					defaultOpenKeys={['3', '4']}
-					activeKey={activeKey}
-					onSelect={handleSelect}
-					appearance='subtle'
-				>
-					<Sidenav.Body>
-						<div className='sidebar-links'>
-							{links.map((link, i) => (
-								<FadeInSection key={i} delay={`${i + 1}00ms`}>
-									<div>{link}</div>
-								</FadeInSection>
-							))}
-						</div>
-					</Sidenav.Body>
+				<Sidenav appearance='subtle'>
+					<div className='sidebar-links'>
+						{links.map((link, i) => (
+							<FadeInSection key={i} delay={`${i + 1}00ms`}>
+								<a href={link.href}>{link.label}</a>
+							</FadeInSection>
+						))}
+					</div>
 				</Sidenav>
 			)}
 			<div className='sidebar-logos'>
